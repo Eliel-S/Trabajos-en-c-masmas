6827c7dd4b7c9ab140c35286b4cc783bc7d43988
Eliel-S <elielsolis09@gmail.com>
2025-04-28 07:52:05 -0600

    Add files via upload

---
+++ suma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+int v1,v2,r;
+std::cout<<"Ingrese su primer valor: ";
+std::cin>>v1;
+std::cout<<"Ingrese su segundo valor: ";
+std::cin>>v2;
+r=v1+v2;
+std::cout<<"El resultado de su suma es: "<<r;
+return 0;
+}
