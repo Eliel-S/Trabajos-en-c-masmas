1272378477aeb4bc6a6800bb80ddd3ba3a50aa05
Solís Eliel <elielsolis09@gmail.com>
2025-04-28 08:39:02 -0600

    Add files via upload

---
+++ división.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+int v1,v2,r;
+std::cout<<"Ingrese su dividendo: ";
+std::cin>>v1;
+std::cout<<"Ingrese su divisor: ";
+std::cin>>v2;
+r=v1/v2;
+std::cout<<"El resultado de su division es: "<<r;
+return 0;
+}
